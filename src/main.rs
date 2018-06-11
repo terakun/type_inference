04f664dead73de6d52b340b9c9aec90f01ed4923
Terasaki Shun <t.tera1217@gmail.com>
2018-06-11 13:48:30 +0900

    implement unification

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,73 @@ enum Const {
     Bool(bool),
 }
 
+impl ToString for Const {
+    fn to_string(&self) -> String {
+        match *self {
+            Const::Int(ref n) => n.to_string(),
+            Const::Bool(ref b) => b.to_string(),
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 enum Decl {
     Var(String, Box<Expr>),
     Func(String, Vec<String>, Box<Expr>),
 }
 
+impl ToString for Decl {
+    fn to_string(&self) -> String {
+        match *self {
+            Decl::Var(ref x, ref e) => format!("{} := {}", x, e.to_string()),
+            Decl::Func(ref f, ref ops, ref e) => {
+                let ops_str: Vec<String> = ops.iter().map(|op| op.to_string()).collect();
+                format!("{}({}) := {}", f, ops_str.join(","), e.to_string())
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 enum Op {
     Plus,
     Lt,
+}
+
+impl ToString for Op {
+    fn to_string(&self) -> String {
+        match *self {
+            Op::Plus => "+".to_string(),
+            Op::Lt => "<".to_string(),
+        }
+    }
+}
+
+impl ToString for Expr {
+    fn to_string(&self) -> String {
+        use Expr::*;
+        match *self {
+            Var(ref x) => x.to_string(),
+            Constant(ref c) => c.to_string(),
+            If(ref e_cond, ref e_then, ref e_else) => format!(
+                "if {} then {} else {}",
+                e_cond.to_string(),
+                e_then.to_string(),
+                e_else.to_string()
+            ),
+            Func(ref f, ref ops) => {
+                let ops_str: Vec<String> = ops.iter().map(|op| op.to_string()).collect();
+                format!("{}({})", f, ops_str.join(","))
+            }
+            Let(ref decl, ref e) => format!("let {} in {}", decl.to_string(), e.to_string()),
+            BinOp(ref op, ref e_left, ref e_right) => format!(
+                "{} {} {}",
+                e_left.to_string(),
+                op.to_string(),
+                e_right.to_string()
+            ),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -69,9 +126,6 @@ type TypeEnv = HashMap<String, Type>;
 type Equality = (Type, Type);
 type Constraints = VecDeque<Equality>;
 
-// type VarExpr = (String, Type);
-// type Unifier = Vec<VarExpr>;
-
 fn merge(c_dst: &mut Constraints, mut c_src: Constraints) {
     while !c_src.is_empty() {
         let c = c_src.pop_front().unwrap();
@@ -79,7 +133,7 @@ fn merge(c_dst: &mut Constraints, mut c_src: Constraints) {
     }
 }
 
-fn Op2Type(op: &Op) -> Type {
+fn op2type(op: &Op) -> Type {
     match *op {
         Op::Plus => Type::Func(
             vec![Type::Base(BaseType::Int), Type::Base(BaseType::Int)], // (Int,Int) -> Int
@@ -128,7 +182,7 @@ impl TypeInf {
 
                 let alpha = self.get_type_variable();
                 c.push_back((
-                    Op2Type(&op),
+                    op2type(&op),
                     Type::Func(vec![t_left, t_right], Box::new(alpha.clone())),
                 ));
                 (alpha, c)
@@ -199,11 +253,94 @@ impl TypeInf {
     }
 }
 
+type VarType = (String, Type);
+type Unifier = Vec<VarType>;
+
+impl Type {
+    fn unify(&self, u: &VarType) -> Self {
+        use Type::*;
+        match *self {
+            Var(ref s) => if *s == u.0 {
+                u.1.clone()
+            } else {
+                self.clone()
+            },
+            Base(_) => self.clone(),
+            Func(ref ops, ref v) => {
+                let unified_ops: Vec<Type> = ops.iter().map(|op| op.unify(u)).collect();
+                let unified_v = v.unify(u);
+                Func(unified_ops, Box::new(unified_v))
+            }
+        }
+    }
+    fn appear(&self, x: &String) -> bool {
+        use Type::*;
+        match *self {
+            Var(ref s) => s == x,
+            Base(_) => false,
+            Func(ref ops, ref v) => {
+                for t in ops {
+                    if t.appear(x) {
+                        return true;
+                    }
+                }
+                v.appear(x)
+            }
+        }
+    }
+}
+
+fn unify(constraints: &mut Constraints, u: &VarType) -> Constraints {
+    let mut unified_constraints = Constraints::new();
+    while !constraints.is_empty() {
+        let constraint = constraints.pop_front().unwrap();
+        unified_constraints.push_back((constraint.0.unify(&u), constraint.1.unify(&u)));
+    }
+    return unified_constraints;
+}
+
+fn calculate_mgu(constraints: &Constraints) -> Option<Unifier> {
+    use Type::*;
+    let mut constraints = constraints.clone();
+    let mut unifier = Unifier::new();
+    while !constraints.is_empty() {
+        let constraint = constraints.pop_front().unwrap();
+        match constraint {
+            (Var(x), t) | (t, Var(x)) => {
+                if t.appear(&x) {
+                    return None;
+                } else {
+                    let u = (x, t);
+                    constraints = unify(&mut constraints, &u);
+                    unifier.push(u);
+                }
+            }
+            (Func(ops1, v1), Func(ops2, v2)) => {
+                if ops1.len() == ops2.len() {
+                    for e in ops1.iter().map(|t| t.clone()).zip(ops2) {
+                        constraints.push_back(e);
+                    }
+                    constraints.push_back((*v1, *v2));
+                } else {
+                    return None;
+                }
+            }
+            _ => {
+                if constraint.0 != constraint.1 {
+                    return None;
+                }
+            }
+        }
+    }
+    Some(unifier)
+}
+
 fn main() {
     // let x = 15 in let f(y,z) = y+z in if 0 < x then x else f(10,x)
     let expr_x = Expr::Var("x".to_string());
     let expr_y = Expr::Var("y".to_string());
     let expr_z = Expr::Var("z".to_string());
+    let expr_varf = Expr::Var("f".to_string());
     let expr_15 = Expr::Constant(Const::Int(15));
     let expr_10 = Expr::Constant(Const::Int(10));
     let expr_0 = Expr::Constant(Const::Int(0));
@@ -223,13 +360,17 @@ fn main() {
     let expr_let1 = Expr::Let(decl_f, Box::new(expr_if));
     let decl_x = Decl::Var("x".to_string(), Box::new(expr_15));
     let expr = Expr::Let(decl_x, Box::new(expr_let1));
-
+    println!("{}", expr.to_string());
     let mut tf = TypeInf::new();
     let tenv = TypeEnv::new();
     let (t, constraints) = tf.type_inf(&tenv, &expr);
-    println!("{:?}", t);
-    println!("{:?}", constraints);
-    // let unifier = calculate_mgu(&constraints).expect("error");
-    // let t = unifier.unify(t);
     // println!("{:?}", t);
+    // println!("{:?}", constraints);
+    let unifier = calculate_mgu(&constraints).expect("error");
+    // println!("{:?}", unifier);
+    let mut well_typed = t;
+    for u in unifier {
+        well_typed = well_typed.unify(&u);
+    }
+    println!("{}", well_typed.to_string());
 }
